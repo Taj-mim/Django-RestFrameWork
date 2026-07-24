049e9b8dd376ba4b71c9ee45f2a80a54cdc8616a
Taj-mim <tajmim915@gmail.com>
2026-07-25 02:37:13 +0600

    Add manual serializer for query the dynamic data

--- DRF_Project/api/views.py
+++ DRF_Project/api/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from Student_app.models import Student
 
 # Create your views here.
 def studentdoc(request):
-    details={
-        'id' : 1,
-        'name' :'fatema',
-        'roll' :12,
-    }
-    return JsonResponse(details)
+    
+    details=Student.objects.all()
+    student_list=list(details.values())
+    return JsonResponse(student_list, safe=False)
