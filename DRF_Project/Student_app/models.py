33e680efe8f09b2d8e882869b3bec55c0553f4d1
Taj-mim <tajmim915@gmail.com>
2026-07-25 02:12:35 +0600

    Through changes created model and register the site in admin.py for dynamic data

--- DRF_Project/Student_app/models.py
+++ DRF_Project/Student_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    id = models.AutoField(primary_key=True)
+    name=models.CharField(max_length=255,default="Unknown")
+    dept=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
