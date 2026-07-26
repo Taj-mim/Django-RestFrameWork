c8c1b98fde28982868654425cb406f496ec2b392
Taj-mim <tajmim915@gmail.com>
2026-07-27 00:48:13 +0600

    In api view add function based GET view

--- DRF_Project/api/views.py
+++ DRF_Project/api/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+#from django.shortcuts import render
+#from django.http import JsonResponse
 from Student_app.models import Student
-
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
+@api_view(['GET'])
 def studentdoc(request):
-    
-    details=Student.objects.all()
-    student_list=list(details.values())
-    return JsonResponse(student_list, safe=False)
+    if request.method == 'GET':
+        #get all the student details
+        details=Student.objects.all()
+        serializer=StudentSerializer(details,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
