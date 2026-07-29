f4969b57c1befc884fea696886256fa65e80a443
Taj-mim <tajmim915@gmail.com>
2026-07-29 17:26:11 +0600

    Unintentionally deleted virtual environment,for that again install it and perform post

--- DRF_Project/api/views.py
+++ DRF_Project/api/views.py
@@ -6,10 +6,17 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def studentdoc(request):
     if request.method == 'GET':
         #get all the student details
         details=Student.objects.all()
         serializer=StudentSerializer(details,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    elif(request.method=='POST'):
+        #Fill up the form
+        serializer=StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
