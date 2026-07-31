79c32bc80e0e8f571105d6acde1bd2220ac20e76
Taj-mim <tajmim915@gmail.com>
2026-07-31 18:26:59 +0600

    Create a employee model then register in along with migration

--- DRF_Project/employeeapp/models.py
+++ DRF_Project/employeeapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class employee(models.Model):
+    id =models.AutoField(primary_key=True)
+    emp_name=models.CharField(max_length=255)
+    emp_dept=models.CharField(max_length=255)
+    emp_designation=models.CharField(max_length=255)
+def __str__(self):
+    return self.name
