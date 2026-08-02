8781c495fdee7c74e76462a5cb61c67f5f847f5d
Taj-mim <tajmim915@gmail.com>
2026-08-02 21:47:03 +0600

    Learn about Viewsets

--- DRF_Project/teacherapp/urls.py
+++ DRF_Project/teacherapp/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-from .views import teacherDetails
-from .views import teacherList
+from django.urls import path ,include
+#from .views import teacherDetails
+#from .views import teacherList
+from .views import teacherViewSet
+from rest_framework.routers import DefaultRouter
+router=DefaultRouter()
+router.register('teacher',teacherViewSet,basename='teacher')
 
 urlpatterns = [
-    path('teacher/',teacherDetails.as_view()),
-    path('teacher/<int:pk>/',teacherList.as_view())
+   # path('teacher/',teacherDetails.as_view()),
+   # path('teacher/<int:pk>/',teacherList.as_view()),
+    path('',include(router.urls))
 ]
--- DRF_Project/teacherapp/views.py
+++ DRF_Project/teacherapp/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics,mixins  
 from .models import teacher
 from .serializers import teacherserializers
+from django.shortcuts import get_object_or_404
 # Create your views here.
 #mixxins are used to add the functionality of the class based views to the generic views
 """
@@ -21,6 +22,9 @@ class teacherList(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.Destr
     def delete(self,request,pk):
         return self.delete(request,pk)
 """
+
+#Generic views are used to reduce the code and make it more readable and easy to understand
+"""
 class teacherDetails(generics.ListCreateAPIView):
     queryset=teacher.objects.all()
     serializer_class=teacherserializers 
@@ -28,3 +32,59 @@ class teacherList(generics.RetrieveUpdateDestroyAPIView):
     queryset=teacher.objects.all()
     serializer_class=teacherserializers
     lookup_field='pk'
+ """
+#viewsets are used to reduce the code and make it more readable and easy to understand
+
+"""
+from rest_framework import viewsets
+from rest_framework.response import Response
+class teacherViewSet(viewsets.ViewSet):
+    def list(self,request):
+        queryset=teacher.objects.all()
+        serializer=teacherserializers(queryset,many=True)
+        return Response(serializer.data)
+    def create(self,request):
+        serializer=teacherserializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    def retrieve(self,request,pk=None):
+        Teacher = get_object_or_404(teacher, pk=pk)
+        serializer = teacherserializers(Teacher)
+        return Response(serializer.data)
+    def update(self,request,pk=None):
+        try:
+            teacher_obj=teacher.objects.get(pk=pk)
+            serializer=teacherserializers(teacher_obj,data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors)
+        except teacher.DoesNotExist:
+            return Response({'error': 'Teacher not found'}, status=404)
+    def destroy(self,request,pk=None):
+        try:
+            teacher_obj=teacher.objects.get(pk=pk)
+            teacher_obj.delete()
+            return Response({'message': 'Teacher deleted successfully'})
+        except teacher.DoesNotExist:
+            return Response({'error': 'Teacher not found'}, status=404)
+    def partial_update(self,request,pk=None):
+        try:
+            teacher_obj=teacher.objects.get(pk=pk)
+            serializer=teacherserializers(teacher_obj,data=request.data,partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors)
+        except teacher.DoesNotExist:
+            return Response({'error': 'Teacher not found'}, status=404) """
+#modelViewSet is used to provide the functionality of the class based views to the viewsets
+from rest_framework import viewsets
+class teacherViewSet(viewsets.ModelViewSet):
+    queryset=teacher.objects.all()
+    serializer_class=teacherserializers
